1612e3481ff523005c32fbe06e230f217d01803c
PrettyFamous <pavelbush2@gmail.com>
2022-11-25 23:59:41 +0300

    Add functional on Sort and Ordering

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-
-const Categories = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
+const Categories = ({ value, onChangeCategory }) => {
   const categories = [
     "Все",
     "Мясные",
@@ -15,13 +11,13 @@ const Categories = () => {
   return (
     <div className="categories">
       <ul>
-        {categories.map((value, index) => (
+        {categories.map((categoryName, index) => (
           <li
             key={index}
-            onClick={() => setActiveIndex(index)}
-            className={activeIndex === index ? "active" : ""}
+            onClick={() => onChangeCategory(index)}
+            className={value === index ? "active" : ""}
           >
-            {value}
+            {categoryName}
           </li>
         ))}
       </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,22 @@ import Pagination from "../components/Pagination";
 const Home = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [categoryId, setCategoryId] = useState(0);
+  const [orderDesc, setOrderDesc] = useState(false);
+  const [sortType, setSortType] = useState({
+    name: "популярности",
+    sortProperty: "rating",
+  });
 
   useEffect(() => {
-    fetch("https://637e0893cfdbfd9a63a4e9c0.mockapi.io/items")
+    setIsLoading(true);
+    fetch(
+      `https://637e0893cfdbfd9a63a4e9c0.mockapi.io/items?sortBy=${
+        sortType.sortProperty
+      }&order=${orderDesc ? "desc" : "asc"}${
+        categoryId > 0 ? "&category=" + categoryId : ""
+      }`
+    )
       .then((response) => response.json())
       .then((arr) => {
         setItems(arr);
@@ -19,19 +32,24 @@ const Home = () => {
       });
 
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType, orderDesc]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onChangeCategory={setCategoryId} />
+        <Sort
+          value={sortType}
+          onChangeSort={setSortType}
+          orderDesc={orderDesc}
+          setOrderDesc={setOrderDesc}
+        />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {isLoading
           ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+          : items.map((item) => <PizzaBlock key={item.id} {...item} />)}
       </div>
       <Pagination />
     </div>
